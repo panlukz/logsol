61d1a3dabacf6139ac05151b76b47226b3cd37ff
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-10-29 15:22:28 +0100

    add tracking classes

---
+++ LogisticSolutions/Models/PostalAddress.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogisticSolutions.Models
+{
+    public class PostalAddress
+    {
+    }
+}
---
+++ LogisticSolutions/Models/TrackingStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogisticSolutions.Models
+{
+    public class TrackingStatus
+    {
+        public DateTime DateTime { get; set; }
+        public string Location { get; set; }
+        public TrackingStatusEnum Status { get; set; }
+        public string Author { get; set; }
+    }
+}
