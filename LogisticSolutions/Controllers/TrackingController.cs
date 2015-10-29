ccb2494566a7c80dad0b9e1fcd5ae292be71f8db
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-10-29 16:15:39 +0100

    add mockup delivery data

--- LogisticSolutions/Controllers/TrackingController.cs
+++ LogisticSolutions/Controllers/TrackingController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LogisticSolutions.Models;
 
 namespace LogisticSolutions.Controllers
 {
     public class TrackingController : Controller
     {
         // GET: Tracking
-        public ActionResult Index()
+        public ActionResult Index(string deliveryNumber)
         {
-            return View();
+
+            var pickupAddress = new PostalAddress("Krzyżowa 22/1 42-123 Warszawa");
+            var destinationAddress = new PostalAddress("Warszawska 31/2 43-122 Gdańsk");
+            var delivery = new Delivery(pickupAddress, destinationAddress);
+
+            delivery.AddTrackingPoint(TrackingStatusEnum.PickedUpFromSender);
+
+            var deliveryTrackingHistory = delivery.TrackingHistory;
+
+            return View(deliveryTrackingHistory);
         }
     }
 }
