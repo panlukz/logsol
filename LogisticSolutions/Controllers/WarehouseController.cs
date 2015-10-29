717d8d8b3435e0145f25438909bab62f8acfa51e
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-10-29 22:57:51 +0100

    Add WarehouseController

---
+++ LogisticSolutions/Controllers/WarehouseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LogisticSolutions.Models;
+
+namespace LogisticSolutions.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        // GET: Warehouse
+        public ActionResult WarehouseReceipt(Delivery delivery)
+        {
+            return View();
+        }
+
+        public ActionResult WarehouseRelease(Delivery delivery)
+        {
+            return View();
+        }
+    }
+}
