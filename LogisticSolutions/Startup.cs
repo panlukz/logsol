530b6d6d251c4706194e851aaa9335f66383ea57
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-10-29 13:46:44 +0100

    first commit

---
+++ LogisticSolutions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogisticSolutions.Startup))]
+namespace LogisticSolutions
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
