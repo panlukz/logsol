4cf17fa8ed8dbb95c5291881669f7c4bbcd2b624
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-12-25 22:31:23 +0100

    Remove dataFactory injection

--- LogisticSolutions/Controllers/DeliveryController.cs
+++ LogisticSolutions/Controllers/DeliveryController.cs
@@ -1,25 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using LogisticSolutions.DAL;
+﻿using System.Web.Mvc;
 using LogisticSolutions.Interfaces;
 using LogisticSolutions.Models;
-using LogisticSolutions.Services;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
 
 namespace LogisticSolutions.Controllers
 {
     public class DeliveryController : Controller
     {
-        private readonly IDataFactory _dataFactory;
         private readonly IDeliveryService _deliveryService;
 
-        public DeliveryController(IDataFactory dataFactory, IDeliveryService deliveryService)
+        public DeliveryController(IDeliveryService deliveryService)
         {
-            _dataFactory = dataFactory;
             _deliveryService = deliveryService;
         }
 
