477ec8939c4b45f86d69b4c925fbc745ab1b384b
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-12-28 17:07:45 +0100

    Add unobtrusive ajax to bundles

--- LogisticSolutions/App_Start/BundleConfig.cs
+++ LogisticSolutions/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace LogisticSolutions
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryuno").Include(
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +29,7 @@ namespace LogisticSolutions
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
