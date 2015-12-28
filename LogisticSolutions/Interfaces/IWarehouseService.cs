3753599acc4a13095763ae79d02a42445bfd723e
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-12-28 17:06:47 +0100

    Add IWarehouse

---
+++ LogisticSolutions/Interfaces/IWarehouseService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using LogisticSolutions.Models;
+
+namespace LogisticSolutions.Interfaces
+{
+    public interface IWarehouseService
+    {
+        bool WarehouseRelease(Delivery delivery);
+        bool WarehouseReceipt(Delivery delivery);
+        IEnumerable<Delivery> GetAllDeliveries();
+    }
+}
