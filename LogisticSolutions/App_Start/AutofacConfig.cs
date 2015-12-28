856c947d25db7f0774a5e3dfc8d5b3c5ad873d72
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-12-28 17:08:37 +0100

    Add courier service dependency

--- LogisticSolutions/App_Start/AutofacConfig.cs
+++ LogisticSolutions/App_Start/AutofacConfig.cs
@@ -16,6 +16,7 @@ namespace LogisticSolutions
             builder.RegisterType<DataContext>().As<IDataContext>().InstancePerRequest();
             builder.RegisterType<DataFactory>().As<IDataFactory>().InstancePerRequest();
             builder.RegisterType<DeliveryService>().As<IDeliveryService>().InstancePerRequest();
+            builder.RegisterType<CourierService>().As<ICourierService>().InstancePerRequest();
             builder.RegisterControllers(Assembly.GetExecutingAssembly());
             DependencyResolver.SetResolver(new AutofacDependencyResolver(builder.Build()));
         } 
