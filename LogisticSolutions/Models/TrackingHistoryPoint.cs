d0de76e0b81ed9e62cab237894a15a253155becb
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-12-30 23:08:06 +0100

    Add TrackingStatus Id generated by database identity

--- LogisticSolutions/Models/TrackingHistoryPoint.cs
+++ LogisticSolutions/Models/TrackingHistoryPoint.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LogisticSolutions.Models
 {
     public class TrackingHistoryPoint
     {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public DateTime DateTime { get; set; }
