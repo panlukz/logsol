471ceb04682535248ea1cebdda0129cf44740eb2
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-12-30 20:21:16 +0100

    Add GetDisplayValue helper method

---
+++ LogisticSolutions/Helpers/EnumHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LogisticSolutions.Helpers
+{
+    public static class EnumHelper
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            var type = value.GetType();
+            if (!type.IsEnum) throw new ArgumentException(String.Format("Type '{0}' is not Enum", type));
+
+            var members = type.GetMember(value.ToString());
+            if (members.Length == 0)
+                throw new ArgumentException(String.Format("Member '{0}' not found in type '{1}'", value, type.Name));
+
+            var member = members[0];
+            var attributes = member.GetCustomAttributes(typeof (DisplayAttribute), false);
+            if (attributes.Length == 0)
+                throw new ArgumentException(String.Format("'{0}.{1}' doesn't have DisplayAttribute", type.Name, value));
+
+            var attribute = (DisplayAttribute) attributes[0];
+            return attribute.GetName();
+        }
+    }
+}
