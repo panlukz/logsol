8852f708d23813e3389a005dc440640afac60ba8
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2016-01-02 22:40:14 +0100

    Add TrackingHistoryPointViewModel

---
+++ LogisticSolutions/Models/TrackingHistoryPointViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogisticSolutions.Models
+{
+    public class TrackingHistoryPointViewModel
+    {
+        public DateTime DateTime { get; set; }
+        public string Location { get; set; }
+        public TrackingStatus Status { get; set; }
+        public string Author { get; set; }
+    }
+}
