c76ebbd6829512cac3a83115e9848fc548a6ad00
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2016-01-03 00:38:37 +0100

    Add DeliveryViewModel

---
+++ LogisticSolutions/Models/DeliveryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LogisticSolutions.Models
+{
+    public class DeliveryViewModel
+    {
+        public string Number { get; set; }
+        public PostalAddress PickupAddress { get; set; }
+        public PostalAddress DestinationAddress { get; set; }
+        public TrackingStatus Status { get; set; }
+    }
+}
