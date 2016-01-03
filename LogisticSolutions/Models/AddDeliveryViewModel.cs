935f6d3b22fc9779753cb2f38e269a2260952758
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2016-01-03 17:31:08 +0100

    Add AddDeliveryViewModel

---
+++ LogisticSolutions/Models/AddDeliveryViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LogisticSolutions.Models
+{
+    public class AddDeliveryViewModel
+    {
+        public decimal Weight { get; set; }
+
+        public decimal Height { get; set; }
+
+        public decimal Width { get; set; }
+
+        public decimal Length { get; set; }
+
+        public DateTime ReceiptDate { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+
+        public string Content { get; set; }
+
+        public string AdditionalInfo { get; set; }
+
+        public PostalAddress PickupAddress { get; set; }
+
+        public PostalAddress DestinationAddress { get; set; }
+
+    }
+}
