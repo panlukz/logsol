2aedf4cb9b0efeece67d62dc158108e63d2e6861
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2016-01-03 17:30:43 +0100

    Add some additional properties

--- LogisticSolutions/Models/Delivery.cs
+++ LogisticSolutions/Models/Delivery.cs
@@ -19,6 +19,22 @@ namespace LogisticSolutions.Models
 
         public string SenderId { get; set; }
 
+        public decimal Weight { get; set; }
+
+        public decimal Height { get; set; }
+
+        public decimal Width { get; set; }
+
+        public decimal Length { get; set; }
+
+        public DateTime ReceiptDate { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+
+        public string Content { get; set; }
+
+        public string AdditionalInfo { get; set; }
+
         public PostalAddress PickupAddress { get; set; }
 
         public PostalAddress DestinationAddress { get; set; }
