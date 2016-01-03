28c6ad7720efa0a377457301ac2420047d905ccc
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2016-01-03 15:20:05 +0100

    Add some properties

--- LogisticSolutions/Models/PostalAddress.cs
+++ LogisticSolutions/Models/PostalAddress.cs
@@ -12,6 +12,14 @@ namespace LogisticSolutions.Models
             
         }
 
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string ContactPerson { get; set; }
+
         public string AddressLine1 { get; set; }
 
         public string AddressLine2 { get; set; }
