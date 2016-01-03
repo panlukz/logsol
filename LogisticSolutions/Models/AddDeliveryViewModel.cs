fb05552859a6eebe9e52279b2065527934c6bd79
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2016-01-03 21:46:52 +0100

    Replace hardcoded field names with DisplayName attributes

--- LogisticSolutions/Models/AddDeliveryViewModel.cs
+++ LogisticSolutions/Models/AddDeliveryViewModel.cs
@@ -5,34 +5,42 @@ namespace LogisticSolutions.Models
 {
     public class AddDeliveryViewModel
     {
-        [Required(ErrorMessage = "Podanie wagi jest wymagane!")]
-        [Range(1,1000, ErrorMessage = "Waga musi zawierać się pomiędzy {1} a {2}")]
+        [Display(Name = "Waga")]
+        [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
+        [Range(1, 1000, ErrorMessage = "Wartość pola {0} musi zawierać się pomiędzy {1} a {2}")]
         public decimal Weight { get; set; }
 
-        [Required(ErrorMessage = "Podanie wysokości jest wymagane!")]
-        [Range(1, 200, ErrorMessage = "Wysokośc musi zawierać się pomiędzy {1} a {2}")]
+        [Display(Name = "Wysokość")]
+        [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
+        [Range(1, 200, ErrorMessage = "Wartość pola {0} musi zawierać się pomiędzy {1} a {2}")]
         public decimal Height { get; set; }
 
-        [Required(ErrorMessage = "Podanie szerokości jest wymagane!")]
-        [Range(1, 1500, ErrorMessage = "Szerokość musi zawierać się pomiędzy {1} a {2}")]
+        [Display(Name = "Szerokość")]
+        [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
+        [Range(1, 1500, ErrorMessage = "Wartość pola {0} musi zawierać się pomiędzy {1} a {2}")]
         public decimal Width { get; set; }
 
-        [Required(ErrorMessage = "Podanie długości jest wymagane!")]
-        [Range(1, 3000, ErrorMessage = "Długość musi zawierać się pomiędzy {1} a {2}")]
+        [Display(Name = "Długość")]
+        [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
+        [Range(1, 3000, ErrorMessage = "Wartość pola {0} musi zawierać się pomiędzy {1} a {2}")]
         public decimal Length { get; set; }
 
-        [Required(ErrorMessage = "Podanie daty odbioru jest wymagane!")]
+        [Display(Name = "Data odbioru")]
+        [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
         [DataType(DataType.Date)]
         public DateTime ReceiptDate { get; set; }
 
-        [Required(ErrorMessage = "Podanie daty dostawy jest wymagane!")]
+        [Display(Name = "Data dostawy")]
+        [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
         [DataType(DataType.Date)]
         public DateTime DeliveryDate { get; set; }
 
-        [StringLengthAttribute(100, MinimumLength = 0, ErrorMessage = "Maksymalna długość opisu zawartości wynosi 100 znaków!")]
+        [Display(Name = "Zawartość")]
+        [StringLengthAttribute(100, MinimumLength = 0, ErrorMessage = "Maksymalna wartość pola {0} wynosi 100 znaków!")]
         public string Content { get; set; }
 
-        [StringLengthAttribute(200, MinimumLength = 0, ErrorMessage = "Maksymalna długość dodatowych informacji wynosi 200 znaków!")]
+        [Display(Name = "Dodatkowe informacje")]
+        [StringLengthAttribute(200, MinimumLength = 0, ErrorMessage = "Maksymalna wartość pola {0} wynosi 200 znaków!")]
         public string AdditionalInfo { get; set; }
 
         public PostalAddress PickupAddress { get; set; }
