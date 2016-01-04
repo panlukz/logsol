542d369e52b1b184d9faa8efddefa711a8e25032
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2016-01-04 21:34:10 +0100

    Add some DataAnnotations atributes

--- LogisticSolutions/Models/AddDeliveryViewModel.cs
+++ LogisticSolutions/Models/AddDeliveryViewModel.cs
@@ -36,93 +36,97 @@ namespace LogisticSolutions.Models
         public DateTime DeliveryDate { get; set; }
 
         [Display(Name = "Zawartość")]
-        [StringLengthAttribute(100, MinimumLength = 0, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(100, MinimumLength = 0, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string Content { get; set; }
 
         [Display(Name = "Dodatkowe informacje")]
-        [StringLengthAttribute(200, MinimumLength = 0, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(200, MinimumLength = 0, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string AdditionalInfo { get; set; }
 
 
 
         [Display(Name = "Nazwa nadawcy")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(50, MinimumLength = 9, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(50, MinimumLength = 9, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string PickupAddressName { get; set; }
 
         [Display(Name = "Telefon")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(20, MinimumLength = 9, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(20, MinimumLength = 9, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string PickupAddressPhone { get; set; }
 
         [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Wprowadź poprawny adres email!")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(50, MinimumLength = 5, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(50, MinimumLength = 5, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string PickupAddressEmail { get; set; }
 
         [Display(Name = "Osoba kontaktowa")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(50, MinimumLength = 9, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(50, MinimumLength = 9, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string PickupAddressContactPerson { get; set; }
 
         [Display(Name = "Adres")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(20, MinimumLength = 5, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(20, MinimumLength = 5, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string PickupAddressAddressLine1 { get; set; }
 
         [Display(Name = "Adres cd.")]
-        [StringLengthAttribute(20, MinimumLength = 0, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(20, MinimumLength = 0, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string PickupAddressAddressLine2 { get; set; }
 
         [Display(Name = "Kod pocztowy")]
+        [RegularExpression("^\\d{2}-\\d{3}$", ErrorMessage = "Wprowadź poprawny kod pocztowy w formacie xx-xxx")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(6, MinimumLength = 6, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(6, MinimumLength = 6, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string PickupAddressPostalCode { get; set; }
 
         [Display(Name = "Miasto")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(30, MinimumLength = 1, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(30, MinimumLength = 1, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string PickupAddressCity { get; set; }
+
 
 
         [Display(Name = "Nazwa nadawcy")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(50, MinimumLength = 9, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(50, MinimumLength = 9, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string DestinationAddressName { get; set; }
 
         [Display(Name = "Telefon")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(20, MinimumLength = 9, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(20, MinimumLength = 9, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string DestinationAddressPhone { get; set; }
 
         [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Wprowadź poprawny adres email!")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(50, MinimumLength = 5, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(50, MinimumLength = 5, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string DestinationAddressEmail { get; set; }
 
         [Display(Name = "Osoba kontaktowa")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(50, MinimumLength = 9, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(50, MinimumLength = 9, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string DestinationAddressContactPerson { get; set; }
 
         [Display(Name = "Adres")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(20, MinimumLength = 5, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(20, MinimumLength = 5, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string DestinationAddressAddressLine1 { get; set; }
 
         [Display(Name = "Adres cd.")]
-        [StringLengthAttribute(20, MinimumLength = 0, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(20, MinimumLength = 0, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string DestinationAddressAddressLine2 { get; set; }
 
         [Display(Name = "Kod pocztowy")]
+        [RegularExpression("^\\d{2}-\\d{3}$", ErrorMessage = "Wprowadź poprawny kod pocztowy w formacie xx-xxx")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(6, MinimumLength = 6, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(6, MinimumLength = 6, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string DestinationAddressPostalCode { get; set; }
-
 
         [Display(Name = "Miasto")]
         [Required(ErrorMessage = "Uzupełnienie pola {0} jest wymagane!")]
-        [StringLengthAttribute(30, MinimumLength = 1, ErrorMessage = "Maksymalna wartość pola {0} wynosi {2} znaków!")]
+        [StringLengthAttribute(30, MinimumLength = 1, ErrorMessage = "{0}: wprowadź pomiędzy {2} a {1} znaków!")]
         public string DestinationAddressCity { get; set; }
 
     }
