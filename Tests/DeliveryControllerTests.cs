0147a15e8fcaef95d4c7e503487deee38bd98857
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2016-01-18 22:10:05 +0100

    Add Moq package

---
+++ Tests/DeliveryControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using LogisticSolutions.Interfaces;
+using LogisticSolutions.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    class DeliveryControllerTests
+    {
+
+        [Test]
+        public void ShouldRegisterDelivery()
+        {
+            var deliveryServiceMock = new Mock<IDeliveryService>();
+            
+        }
+
+    }
+}
