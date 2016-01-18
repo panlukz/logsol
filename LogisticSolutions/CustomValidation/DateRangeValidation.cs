2e7d9692ad51658a3b585fc082241dbb1fbdf612
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2016-01-18 21:15:55 +0100

    Add jquery datepicker

---
+++ LogisticSolutions/CustomValidation/DateRangeValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LogisticSolutions.CustomValidation
+{
+    public class DateRangeValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var specifiedDate = Convert.ToDateTime(value);
+            var earliestPossibleDate = DateTime.Today;
+
+            if (DateTime.Now.Hour > 12)
+            {
+                earliestPossibleDate.AddDays(1);
+            }
+
+            if (specifiedDate >= earliestPossibleDate)
+            {
+                return ValidationResult.Success;
+            }
+            
+            return new ValidationResult("Wybrana data jest poza przedziałem");
+        }
+    }
+}
